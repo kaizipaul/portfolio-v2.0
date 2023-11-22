83c65cc770bf2144394481d60e44401758ec8cba
kaizipaul <kaizipaul@gmail.com>
2023-11-22 03:06:09 +0300

    Update about section and experience section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import 'nextra-theme-blog/style.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import '../styles/main.css'
+import { ChakraProvider } from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -21,7 +22,9 @@ export default function App({ Component, pageProps }: AppProps) {
           crossOrigin="anonymous"
         />
       </Head>
+      <ChakraProvider>
       <Component {...pageProps} />
+      </ChakraProvider>
     </>
   )
 }
